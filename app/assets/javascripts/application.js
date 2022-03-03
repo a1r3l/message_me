8ab6b3ad90f307a90e335940893927e200fbb21d
Aurel Ioan Patrutiu <aurelioan93@gmail.com>
2022-03-03 01:09:02 +0100

    Add Flash messages to ChatApp

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,9 @@
 //= require semantic-ui
 
 $(document).on('turbolinks:load', function(){
-    $('.ui.dropdown')
-    .dropdown()
-  ;
+    $('.ui.dropdown').dropdown();
+
+    $('.message .close').on('click', function() {
+      $(this).closest('.message').transition('fade');
+    });
 });
